6f1148debae4002ae8280b2c8b8dad594c33b76e
DESKTOP-IKS85IO\user <rladyddn258@gmail.com>
2023-09-26 13:25:55 +0900

    초기상태 커밋

---
+++ DSME_HanhwaOcean/MainWindow.xaml.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.Extensions.DependencyInjection;
+using System.Runtime.InteropServices;
+using System.Windows;
+
+namespace DSME_HanhwaOcean
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var serviceCollection = new ServiceCollection();
+            serviceCollection.AddWpfBlazorWebView();
+            Resources.Add("services", serviceCollection.BuildServiceProvider());
+            NativeMethods.AllocConsole();
+        }
+    }
+
+    static class NativeMethods
+    {
+        [DllImport("kernel32.dll", SetLastError = true)]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        public static extern bool AllocConsole();
+    }
+}
